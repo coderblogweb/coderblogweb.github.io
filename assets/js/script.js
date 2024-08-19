9b78b003dfb8c3fb5b6f9b4b7d2ddb9905bd9a72
Coder Blog <131983598+coderblogweb@users.noreply.github.com>
2024-08-19 09:40:10 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,31 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+
+
+
+/**
+ * Contact Form Script
+ */
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxDJF2M4PpjjtTCkSn0CO6jnmsMNQw3xojDtEEk9MtYiNoQ9X9dlHvaOq05CDz_tM8/exec';
+
+const form = document.forms['submit-to-google-sheet'];
+
+const msg = document.getElementById('msg');
+
+form.addEventListener('submit', e => {
+
+  e.preventDefault()
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message Sent!!"
+    })
+
+    setTimeout(function() {
+      msg.innerHTML = ""
+    }, 10000)
+    form.reset()
+    .catch(error => console.error('Error!', error.message))
+});
